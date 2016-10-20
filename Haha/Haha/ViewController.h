bb5841acea36531e4e986eb0216344da94252015
金鑫亮 <jinxinliang@lanou3g.com>
2016-10-20 11:02:01 +0800

    初始化工程

---
+++ Haha/Haha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Haha
+//
+//  Created by Eiwodetianna on 16/10/20.
+//  Copyright © 2016年 Eiwodetianna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Haha/Haha/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Haha
+//
+//  Created by Eiwodetianna on 16/10/20.
+//  Copyright © 2016年 Eiwodetianna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
