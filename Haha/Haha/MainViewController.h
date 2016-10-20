86a4a54c9449105d3a83422e5abd084e6a3eaeba
金鑫亮 <jinxinliang@lanou3g.com>
2016-10-20 11:15:19 +0800

    A main

---
+++ Haha/Haha/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Haha
+//
+//  Created by Eiwodetianna on 16/10/20.
+//  Copyright © 2016年 Eiwodetianna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
